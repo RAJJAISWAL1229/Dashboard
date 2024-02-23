d4725af3b5eece07ee810d146f2ef7a6a5222da6
Prince Verma <princeverma360p@gmail.com>
2024-02-24 01:00:40 +0530

    css fix

--- script.js
+++ script.js
@@ -3,28 +3,6 @@ google.charts.load("current", { packages: ["corechart", "bar"] });
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-  var data = new google.visualization.DataTable();
-  data.addColumn("string", "Topping");
-  data.addColumn("number", "Slices");
-  data.addRows([
-    ["Mushrooms", 3],
-    ["Onions", 1],
-    ["Olives", 1],
-    ["Zucchini", 1],
-    ["Pepperoni", 2],
-  ]);
-
-  var options = {
-    title: "How Much Pizza I Ate Last Night",
-    width: 400,
-    height: 300,
-  };
-
-  var chart = new google.visualization.PieChart(
-    document.getElementById("chart_div")
-  );
-  chart.draw(data, options);
-
   var data = google.visualization.arrayToDataTable([
     ["Year", "Sales", "Expenses", "Profit"],
     ["2014", 1000, 400, 200],
@@ -38,7 +16,7 @@ function drawChart() {
       title: "Company Performance",
       subtitle: "Sales, Expenses, and Profit: 2014-2017",
     },
-    bars: "horizontal", // Required for Material Bar Charts.
+    bars: "vertical", // Required for Material Bar Charts.
   };
 
   var chart = new google.charts.Bar(
